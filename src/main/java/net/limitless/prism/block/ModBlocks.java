f9724445a62ca83130dd40d45ceb133e0e7aa1fc
SteakTheStake <gabecorr25@gmail.com>
2022-09-04 14:03:20 -0700

    renames and fixes

--- src/main/java/net/limitless/prism/block/ModBlocks.java
+++ src/main/java/net/limitless/prism/block/ModBlocks.java
@@ -4,10 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.limitless.prism.Prism;
 import net.limitless.prism.item.ModItemGroup;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -19,39 +16,18 @@ import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class ModBlocks {
-    public static final Block CASTLE_BLOCK_TOP = registerBlock("castle_block_top",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.STONE);
+    public static final Block CastleBlockTop = registerBlock("castle_block_top",
+            new CastleBlockTop(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.STONE);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
-        registerBlockItem(name, block, tab);
         return Registry.register(Registry.BLOCK, new Identifier(Prism.MOD_ID, name), block);
     }
 
-    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
-        return Registry.register(Registry.ITEM, new Identifier(Prism.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings().group(tab)));
-    }
+
 
     public static void registerModBlocks() {
         Prism.LOGGER.debug("Registering ModBlocks for " + Prism.MOD_ID);
     }
 
-    public class CASTLE_BLOCK_TOP extends Block {
-        public CASTLE_BLOCK_TOP(Settings settings) {
-            super(settings);
-        }
-        @Override
 
-        public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
-
-            return VoxelShapes.union(
-
-                    createCuboidShape(0f, 0f, 0f, 16f, 10f, 16f),
-
-                    createCuboidShape(0f, 10f, 12f, 16f, 16f, 16f),
-
-                    createCuboidShape(0f, 10f, 4f, 16f, 16f, 8f)
-            );
-        }
-    }
 }
--- src/main/java/net/limitless/prism/item/ModItems.java
+++ src/main/java/net/limitless/prism/item/ModItems.java
@@ -2,20 +2,29 @@ package net.limitless.prism.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.limitless.prism.Prism;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    public static final Item CASTLE_BLOCK_TOP = registerItem("castle_block_top",
+    public static final Item CASTLE_BLOCK_TOP = registerBlockItem("castle_block_top",
             new Item(new FabricItemSettings().group(ModItemGroup.STONE)));
 
 
-    private static Item registerItem(String name, Item item) {
+    private static Item registerBlockItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Prism.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         Prism.LOGGER.debug("Registering Mod Items for " + Prism.MOD_ID);
     }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
+        return Registry.register(Registry.ITEM, new Identifier(Prism.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(tab)));
+    }
+
 }
