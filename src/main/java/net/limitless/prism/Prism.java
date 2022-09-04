87a53d7c360a9ed840cb2e16a0960a3553d5540c
SteakTheStake <gabecorr25@gmail.com>
2022-09-04 13:22:43 -0700

    trying to fix errors

--- src/main/java/net/limitless/prism/Prism.java
+++ src/main/java/net/limitless/prism/Prism.java
@@ -2,6 +2,7 @@ package net.limitless.prism;
 
 import net.fabricmc.api.ModInitializer;
 import net.limitless.prism.block.ModBlocks;
+import net.limitless.prism.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,7 @@ public class Prism implements ModInitializer {
 	public void onInitialize() {
 
 		ModBlocks.registerModBlocks();
+		ModItems.registerModItems();
 
 	}
 }
--- src/main/java/net/limitless/prism/block/ModBlocks.java
+++ src/main/java/net/limitless/prism/block/ModBlocks.java
@@ -5,12 +5,18 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.limitless.prism.Prism;
 import net.limitless.prism.item.ModItemGroup;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class ModBlocks {
     public static final Block CASTLE_BLOCK_TOP = registerBlock("castle_block_top",
@@ -28,5 +34,24 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         Prism.LOGGER.debug("Registering ModBlocks for " + Prism.MOD_ID);
+    }
+
+    public class CASTLE_BLOCK_TOP extends Block {
+        public CASTLE_BLOCK_TOP(Settings settings) {
+            super(settings);
+        }
+        @Override
+
+        public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
+
+            return VoxelShapes.union(
+
+                    createCuboidShape(0f, 0f, 0f, 16f, 10f, 16f),
+
+                    createCuboidShape(0f, 10f, 12f, 16f, 16f, 16f),
+
+                    createCuboidShape(0f, 10f, 4f, 16f, 16f, 8f)
+            );
+        }
     }
 }
