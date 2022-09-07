0a48a2dab1f19546a9d72e80767d6a2ea7f873eb
SteakTheStake <gabecorr25@gmail.com>
2022-09-07 13:02:05 -0700

    Update ModItems.java

--- src/main/java/net/limitless/prism/item/ModItems.java
+++ src/main/java/net/limitless/prism/item/ModItems.java
@@ -1,14 +1,5 @@
 package net.limitless.prism.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.limitless.prism.Prism;
-import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
 public class ModItems {
 
     public static void registerItems() {
