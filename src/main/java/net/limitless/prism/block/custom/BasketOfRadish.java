5a5a9a5093ae447eae74eb94daae38a71bdbd4cb
SteakTheStake <gabecorr25@gmail.com>
2022-10-14 22:02:44 -0700

    update

--- src/main/java/net/limitless/prism/block/custom/BasketOfRadish.java
+++ src/main/java/net/limitless/prism/block/custom/BasketOfRadish.java
@@ -1,12 +1,25 @@
 package net.limitless.prism.block.custom;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
 
-public class BasketOfRadish extends Block {
-    public BasketOfRadish(FabricBlockSettings collidable) {
-        super(FabricBlockSettings.of(Material.WOOD));
+public class BasketOfRadish extends HorizontalFacingBlock {
+    public BasketOfRadish(Settings settings) {
+        super(settings);
+        setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
     }
 
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(Properties.HORIZONTAL_FACING);
+    }
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState) this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+    }
 }
